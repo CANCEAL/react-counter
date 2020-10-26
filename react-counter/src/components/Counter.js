b4e4d2d24f341127d756315571fdf1c952815c7e
CANCEAL <alfred.cancejo@oocl.com>
2020-10-26 20:14:39 +0800

    [React] [Alfred] Feature - created Counter.js

---
+++ react-counter/src/components/Counter.js
@@ -0,0 +1,32 @@
+import React, { Component } from 'react';
+
+class Counter extends Component {
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            number: 0,
+        }
+    }
+
+    onIncrease() {
+        this.setState((prevState) => ({number: prevState.number + 1}));
+    }
+
+    onDecrease() {
+        this.setState((prevState) => ({number: prevState.number - 1}));
+    }
+
+    render() {
+        return (
+            <section>
+                <button onClick={() => this.onIncrease()}> + </button>
+                <span> {this.state.number} </span>
+                <button onClick={() => this.onDecrease()}> - </button>
+            </section>
+
+        );
+    }
+}
+
+export default Counter;
